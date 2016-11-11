85a909bf49cb4b96d60090c9e2412201b44bb43c
MatheusSouzaCC <matheussouza@edu.univali.br>
2016-11-11 11:40:18 -0200

    Adicionado wait cursor quando os menus são clicados

--- src/br/com/gestorestoque/view/menu/JItemMenu.java
+++ src/br/com/gestorestoque/view/menu/JItemMenu.java
@@ -1,8 +1,11 @@
 
 package br.com.gestorestoque.view.menu;
 
+import br.com.gestorestoque.util.FRMUtil;
+import br.com.gestorestoque.view.FRMPrincipal;
 import br.com.gestorestoque.view.menu.fabrica.CriadorFrame;
 import br.com.gestorestoque.view.menu.fabrica.EnumFRM;
+import java.awt.Cursor;
 import javax.swing.JDialog;
 import javax.swing.JMenuItem;
 
@@ -30,7 +33,10 @@ public class JItemMenu extends JMenuItem {
         setText(titulo);
 
         addActionListener((e) -> {
+            FRMPrincipal principal = FRMPrincipal.getInstance();
+            principal.setCursor(new Cursor(Cursor.WAIT_CURSOR));
             JDialog janela = CriadorFrame.getFrame(tipoFrame);
+            principal.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
             janela.setVisible(true);
         });
 
